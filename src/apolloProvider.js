5b0112235ef4b52b1a7dbe88b3252751c0055fbb
john katua <johnkatua99@gmail.com>
2020-10-29 22:05:02 +0300

    edited proxy and server base URL

--- src/apolloProvider.js
+++ src/apolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8900'
+  uri: 'https://enigmatic-everglades-82127.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
